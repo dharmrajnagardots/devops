fd0049fa1119d5190309256de3cf6b26753b464f
Dharamraj Nagar <dharamraj.nagar@dotsquares.com>
2023-04-17 14:07:35 +0400

    Jenkins text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Welcome to the jenkins
+          Jenkins <code>src/App.js</code> and save to reload.
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
